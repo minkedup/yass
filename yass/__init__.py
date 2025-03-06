a1693274ce60dbdb9eec97f87ee7e24a06bb445b
Mickey P <me@minkedup.xyz>
2025-03-06 16:41:23 -0500

    Added argparse initialization

--- yass/__init__.py
+++ yass/__init__.py
@@ -2,8 +2,12 @@
 Yet Another (RIT Bus) Schedule Scraper.
 """
 
+import argparse
 
 def main() -> None:
     """
     Parse arguments and run Scraper.
     """
+
+    parser = argparse.ArgumentParser(prog="yass")
+    _args = parser.parse_args()
