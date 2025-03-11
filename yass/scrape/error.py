508f5d8673cf9ddeeef8abb89d10ffa154f78d23
Mickey P <me@minkedup.xyz>
2025-03-11 18:17:28 -0400

    Moved single query checking to error module

--- yass/scrape/error.py
+++ yass/scrape/error.py
@@ -2,8 +2,40 @@
 Scrape Error Types.
 """
 
+from typing import TypeGuard, TypeAlias
+import lxml
+
 
 class ScrapeError(Exception):
     """
     Root Scraping Error.
     """
+
+
+SingleQuery: TypeAlias = lxml.html.Element | None
+
+
+def test_single_query(
+    query: SingleQuery, source: lxml.html.Element, expect_tag: str
+) -> TypeGuard[lxml.html.Element]:
+    """
+    Check that the result of a single element query (e.g. getparent in
+    lxml.html.Element) is not None and matches some expected tag.
+    """
+
+    if query is None:
+        raise ScrapeError(
+            (
+                f"{source.tag} el at line {source.sourceline}"
+                f" has no parent; expected <{expect_tag}>"
+            )
+        ) from None
+    if query.tag != expect_tag:
+        raise ScrapeError(
+            (
+                f"{source.tag} el at line {source.sourceline}"
+                f" has tag {query.tag}; expected <{expect_tag}>"
+            )
+        ) from None
+
+    return True
--- yass/scrape/periods.py
+++ yass/scrape/periods.py
@@ -31,7 +31,7 @@ Assumes that the main page will have the following layout:
 ```
 """
 
-from typing import Sequence, TypeGuard, Optional, cast
+from typing import Sequence, cast
 import re
 import dataclasses
 
@@ -40,7 +40,7 @@ import lxml.html
 from yass.types import ScrapeContext
 from yass.const import ROOT_SCHEDULE_URL
 
-from yass.scrape.error import ScrapeError
+from yass.scrape.error import test_single_query
 from yass.scrape.types import (
     ScrapedPeriodParts,
     ScrapedSubPeriodIdx,
@@ -72,36 +72,16 @@ def _get_parts_grp_div_el_from_period_h3_el(
     in the same group as the wrapper) is what we're after.
     """
 
-    def check_el(
-        query: Optional[lxml.html.Element], source: lxml.html.Element, e_tag: str
-    ) -> TypeGuard[lxml.html.Element]:
-        if query is None:
-            raise ScrapeError(
-                (
-                    f"{source.tag} el at line {source.sourceline}"
-                    f" has no parent; expected <{e_tag}>"
-                )
-            ) from None
-        if query.tag != e_tag:
-            raise ScrapeError(
-                (
-                    f"{source.tag} el at line {source.sourceline}"
-                    f" has tag {query.tag}; expected <{e_tag}>"
-                )
-            ) from None
+    m_h3_div_el = period_h3_el.getparent()
+    test_single_query(m_h3_div_el, period_h3_el, "div")
 
-        return True
-
-    h3_div_el = period_h3_el.getparent()
-    check_el(h3_div_el, period_h3_el, "div")
-
-    pair_grp_div_el = h3_div_el.getparent()
-    check_el(pair_grp_div_el, h3_div_el, "div")
+    m_pair_grp_div_el = m_h3_div_el.getparent()
+    test_single_query(m_pair_grp_div_el, m_h3_div_el, "div")
 
     # NOTE: assert that it is just the wrapper and another thing in the
     # group; we assume that the element we're looking for comes after us
-    assert len(pair_grp_div_el) == 2
-    other_el = pair_grp_div_el[1]
+    assert len(m_pair_grp_div_el) == 2
+    other_el = m_pair_grp_div_el[1]
 
     return other_el
 
